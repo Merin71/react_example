cd18208e3ec92ff462e85ad9c5cd05661305c85f
Merin <merint2000@gmail.com>
2024-02-05 16:39:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import StateBasics from './components/StateBasics';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SampleForm from './components/SampleForm';
 
 function App() {
   return (
     <div className='App'>
+      <BrowserRouter>
       <Routes>
         <Route path='/' element ={<StateBasics/>}/>
         <Route path='/form' element={<SampleForm/>}/>
       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
